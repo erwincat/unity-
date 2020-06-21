b1241181026dd953b0fbffc6eb869054e7751160
wuchangming <wuchangming@MacBook-Pro.local>
2020-06-21 18:45:34 +0800

    第十二章代码

---
+++ ChapterEleven/Assets/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour,IGameManager{
+    public ManagerStatus status { get; private set; }
+    public float soundVolume {
+        get { return AudioListener.volume; }
+        set { AudioListener.volume = value; }
+    }
+
+    public bool soundMute {
+        get { return AudioListener.pause; }
+        set { AudioListener.pause = value; }
+    }
+
+    private NetworkService _network;
+
+    public  void Startup(NetworkService service) {
+        _network = service;
+
+        status = ManagerStatus.Started;
+
+        soundVolume = 1f;
+        status = ManagerStatus.Started;
+    }
+}
---
+++ ChapterEleven/Assets/SettingsPopup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsPopup : MonoBehaviour
+{
+    public void OnSoundToggle() {
+        Managers.Audio.soundMute = !Managers.Audio.soundMute;
+    }
+
+    public void OnSoundValue(float volume) {
+        Managers.Audio.soundVolume = volume;
+    }
+}
